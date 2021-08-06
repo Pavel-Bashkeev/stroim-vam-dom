576a6912830220fd8d9fc2321b017ab8c79e0fd2
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-08-06 21:18:04 +0300

    start

---
+++ app/js/vendor/slick.js
@@ -0,0 +1,12 @@
+$('.slider').slick({
+	dots: true,
+	infinite: true,
+	speed: 500,
+	autoplay: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	fade: true,
+	cssEase: 'linear',
+	prevArrow: '<button type="button" class="slick-prev slick-btn btn-reset"><img src="../images/dest/prevArrow.webp"></button>',
+	nextArrow: '<button type="button" class="slick-next slick-btn btn-reset"><img src="../images/dest/nextArrow.webp"></button>',
+});
