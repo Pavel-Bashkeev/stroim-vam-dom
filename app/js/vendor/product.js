296d56a5ddf0800dc7ee54a5293f203860d702e3
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-08-29 15:56:40 +0300

    Сделан фильтр

--- app/js/vendor/product.js
+++ app/js/vendor/product.js
@@ -1,5 +1,6 @@
 const catalogList = document.querySelector('.catalog-list');
-const catalogMore = document.querySelector('.catalon-more');
+// const catalogMore = document.querySelector('.catalon-more');
+const filterBtns = document.querySelectorAll('.catalog-btns__item');
 let dataLength = null;
 let prodQuantity = 5;
 
@@ -14,19 +15,16 @@ if (catalogList) {
         return response.json()
       })
       .then((data) => {
-        console.log(data);
-
         dataLength = data.length;
-
+        quantity = dataLength;
         catalogList.innerHTML = '';
 
         for (let i = 0; i < dataLength; i++){
           if(i < quantity){
             let item = data[i]
-            console.log(item);
 
             catalogList.innerHTML += `
-            <div class="catalog-list__item">
+            <div class="catalog-list__item" data-filter-item='${item.route}'>
               <p class="catalog-list__item-name">${item.title}</p>
               <div class="catalog-list__item-box">
                 <img src="${item.mainImage}" alt="${item.title}" class="catalog-list__item-img">
@@ -45,26 +43,51 @@ if (catalogList) {
       .then(()=>{
         const modal = new GraphModal({
           isOpen: (modal) => {
-            console.log(modal);
-            console.log('opened');
-
             const openBtn = modal.previousActiveElement;
-            console.log(openBtn);
           },
         });
+
+        
       })
   };
 
-  loadProducts(prodQuantity);
+  loadProducts();
 
-  catalogMore.addEventListener('click', ()=>{
-    prodQuantity = prodQuantity + 1;
-
-    loadProducts(prodQuantity);
-    if(prodQuantity >= dataLength){
-      catalogMore.style.display = 'none'
-    }else {
-      catalogMore.style.display = 'block'
-    }
+  const loadFilterItem = (itemBlock) => {
+    fetch('../data/house.json')
+      .then((response) => {
+        return response.json()
+      })
+      .then(data => {
+        catalogList.innerHTML = '';
+        for (let i = 0; i < dataLength; i++){
+          let item = data[i]
+          if(item.route === itemBlock){
+            catalogList.innerHTML += `
+            <div class="catalog-list__item" data-filter-item='${item.route}'>
+              <p class="catalog-list__item-name">${item.title}</p>
+              <div class="catalog-list__item-box">
+                <img src="${item.mainImage}" alt="${item.title}" class="catalog-list__item-img">
+              </div>
+              <div class="catalog-list__item-size">
+                <p class="catalog-list__item-square">${item.size.square}</p>
+                <p class="catalog-list__item-area">${item.size.area}m<sup>2</sup></p>
+              </div>
+              <p class="catalog-list__item-price">от ${normalPrice(item.price)} р</p>
+              <button class="catalog-list__item-btn" data-graph-path="prod-modal" data-id='${item.id}'>Побробнее</button>
+            </div>
+            `;
+          }
+        }
+      })
+  }
+  window.addEventListener('DOMContentLoaded', ()=>{
+    filterBtns.forEach(item => {
+      item.addEventListener('click', (e)=>{
+        let target = e.target;
+        let filterBtnValue = target.dataset.filterBtn;
+        filterBtnValue == 'all' ? loadProducts(): loadFilterItem(filterBtnValue);
+      })
+    });
   })
 }
